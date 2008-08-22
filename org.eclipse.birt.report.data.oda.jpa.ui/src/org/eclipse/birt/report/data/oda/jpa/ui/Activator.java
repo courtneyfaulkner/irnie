63e0c00536d61b2379c497ffcb01dca57640374b
Victoriano Alfonso Phocco Diaz <vphocco@gmail.com>
2008-08-22 15:26:36 +0000

    git-svn-id: http://birt-jpa-connector.googlecode.com/svn/trunk@31 a90799aa-c650-0410-9101-bdc709519c18

--- org.eclipse.birt.report.data.oda.jpa.ui/src/org/eclipse/birt/report/data/oda/jpa/ui/Activator.java
+++ org.eclipse.birt.report.data.oda.jpa.ui/src/org/eclipse/birt/report/data/oda/jpa/ui/Activator.java
@@ -1,22 +1,50 @@
 package org.eclipse.birt.report.data.oda.jpa.ui;
 
-import org.osgi.framework.BundleActivator;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.birt.report.data.oda.jpa.ui";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
 	 */
-	public void start(BundleContext context) throws Exception {
+	public Activator() {
+		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
 	}
 
 }
